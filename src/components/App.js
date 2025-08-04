68ca7259e960f96c30a049fac02b770d77f27665
Nassur <nassur254@gmail.com>
2025-08-04 11:29:48 +0300

    Components

--- src/components/App.js
+++ src/components/App.js
@@ -1,32 +1,17 @@
 import React from "react";
-
-function NavBar() {
-  return (
-    <nav>
-      <a href="#home">I'm a link!</a>
-    </nav>
-  );
-}
-
-function Home() {
-  return (
-    <div id="home">
-      <h1>Home</h1>
-    </div>
-  );
-}
-
-{/* write an <About> component here */}
-
+import Navbar from './Navbar';
+import Home from './Home';
+import About from './About';
 
 function App() {
   return (
     <div>
-      <NavBar />
-      {/* add the <Home> component here */}
-      {/* add your <About> component here */}
+      <Navbar />   {/* Navbar component */}
+      <Home />     {/* Home component */}
+      <About />    {/* About component */}
     </div>
   );
 }
+
 
 export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+// src/components/Navbar.js
+import React from 'react';
+
+function Navbar() {
+  return (
+    <nav>
+      <a href="#home">I'm a link!</a>
+    </nav>
+  );
+}
+
+export default Navbar;
